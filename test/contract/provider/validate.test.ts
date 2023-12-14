1a0092d533ed94eb5399be7f5e2da4d106f93103
Abshir <abshir.ahmed@ft.com>
2023-12-14 11:44:04 +0000

    use globSync

--- test/contract/provider/validate.test.ts
+++ test/contract/provider/validate.test.ts
@@ -1,17 +1,14 @@
 import { Verifier } from '@pact-foundation/pact';
 import path from 'node:path';
-import { glob } from 'glob';
+import { globSync } from 'glob';
 
 describe('Pact Verification', () => {
   it('validates the expectations of Matching Service', async () => {
-    const pacts = await glob(path.resolve(process.cwd(), './pacts/*.json'));
     const verifier = new Verifier({
       providerBaseUrl: 'http://localhost:3000',
-      pactUrls: pacts
+      pactUrls: globSync(path.resolve(process.cwd(), 'pacts/*.json')) // Finds all pacts in the pacts folder
     });
 
-    return verifier.verifyProvider().then(() => {
-      console.log('Pact Verification Complete ✅');
-    });
+    return verifier.verifyProvider().then(() => console.log('Pact Verification Complete ✅'));
   });
 });
